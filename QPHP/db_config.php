ab5fdbc70dcff41949ad23b6787fbc615bbf2f06
qiji <5709775@gmail.com>
2018-03-18 11:32:58 +0800

    test

--- QPHP/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	'default'=>array(
-        'main' =>array('host'=>'127.0.0.1','database'=>'zblog','port'=>3306,'user'=>'root','password'=>'','charset'=>'utf8'),
-		'query0'=>array('host'=>'127.0.0.1','database'=>'zblog','port'=>3306,'user'=>'root','password'=>'','charset'=>'utf8')
-	)
-);
